058600a84a8d8ac1c5993f603382987ee45eeb77
RexySolehudin <71806091+RexySolehudin@users.noreply.github.com>
2022-04-14 18:21:20 +0700

    product update

--- resources/views/view/product/index.blade.php
+++ resources/views/view/product/index.blade.php
@@ -1,11 +1,55 @@
 @extends('view.index')
 @section('conten')
+    <!-- IKLAM -->
+
+
+
+    <!-- END IKLAN -->
     <!-- Section-->
     <section class="py-5">
         <div class="container px-4 px-lg-5 mt-5">
-            <br>
-            <br>
-            <br>
+        <!-- promo -->
+        <div id="Promo" class="carousel carousel- slide " data-bs-ride="carousel">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#Promo" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#Promo" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#Promo" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active" data-bs-interval="1000">
+                <img src="https://dummyimage.com/600x200/" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>First slide label</h5>
+                  <p>Some representative placeholder content for the first slide.</p>
+                </div>
+              </div>
+              <div class="carousel-item" data-bs-interval="1000">
+                <img src="https://dummyimage.com/600x200/" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Second slide label</h5>
+                  <p>Some representative placeholder content for the second slide.</p>
+                </div>
+              </div>
+              <div class="carousel-item" data-bs-interval="1000">
+                <img src="https://dummyimage.com/600x200/" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Third slide label</h5>
+                  <p>Some representative placeholder content for the third slide.</p>
+                </div>
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+          <br>
+        <!-- end promo -->
+        
             <div class="input-group">
                 <div class="form-text">
                   <input id="search-focus" type="search" id="form1" class="form-control" />
@@ -16,6 +60,7 @@
                   </i>
                 </button>
             </div>
+            <br>
             <div class="input-group">
                 <select class="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">
                   <option selected>Pilih Kategori</option>
@@ -25,8 +70,7 @@
                 </select>
             </div>
             <br>
-            <br>
-            <br>
+          
             <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                 <div class="col mb-5">
                     <div class="card h-100">
