8b51371e52eb5370898eb42d1047d4e9f520f322
RexySolehudin <71806091+RexySolehudin@users.noreply.github.com>
2022-04-20 16:56:42 +0700

    Update home

--- resources/views/view/home/index.blade.php
+++ resources/views/view/home/index.blade.php
@@ -44,6 +44,7 @@
                     <img class="card-img-top" style="height:50%; margin-bottom:2%;" src="{{ asset('user/assets/img/article').'/'.$value->image }}" alt="..." />
                     <h4 class="title"><a href="">{{ $value->title }}</a></h4>
                     <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+					<div class="text-center"><a class="btn btn-outline-success btn-sm mt-auto" href="#">Read More</a></div>
                     </div>
                 </div>
                 @endforeach
@@ -109,9 +110,9 @@
                 <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                     @foreach ($product as $key=>$value)
                     <div class="col mb-5">
-                        <div class="card h-100">
+                        <div class="card h-100 border-gray border-5">
                             <!-- Product image-->
-                            <img class="card-img-top" style="height: 50%;" src="{{ asset('user/assets/img/catalog').'/'.$value->image }}" alt="..." />
+                            <img class="card-img-top border-gray border-5" style="height: 50%;" src="{{ asset('user/assets/img/catalog').'/'.$value->image }}" alt="..." />
                             <!-- Product details-->
                             <div class="card-body p-4">
                                 <div class="text-center">
@@ -123,7 +124,7 @@
                             </div>
                             <!-- Product actions-->
                             <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                                <div class="text-center"><a class="btn btn-outline-success btn-sm mt-auto" href="#">View options</a></div>
                             </div>
                         </div>
                     </div>
