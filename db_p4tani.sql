3495a62f8a8f6dc4c1560ebe8fe15716d6e7edad
oktafian510 <71855139+oktafian510@users.noreply.github.com>
2022-05-28 04:34:42 +0700

    up

--- db_p4tani.sql
+++ db_p4tani.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 27, 2022 at 01:38 PM
+-- Generation Time: May 27, 2022 at 11:34 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -126,8 +126,7 @@ CREATE TABLE `carts` (
 --
 
 INSERT INTO `carts` (`id`, `code`, `id_users`, `nameProduct`, `size`, `quantity`, `price`, `status`, `image`, `created_at`, `updated_at`) VALUES
-(1, '80842', 9, 'Fancy Product', 'standart', 1, 200000.00, 'ready', 'image.jpg', '2022-05-24 11:49:35', '2022-05-24 11:49:35'),
-(2, '80842', 9, 'Fancy Product', 'standart', 11, 200000.00, 'ready', 'image.jpg', '2022-05-24 19:06:08', '2022-05-24 19:06:08');
+(19, '80841', 9, 'Special Item', 'standart', 10, 300000.00, 'ready', 'image.jpg', '2022-05-27 07:57:08', '2022-05-27 07:57:08');
 
 -- --------------------------------------------------------
 
@@ -148,10 +147,12 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `name`, `description`, `created_at`, `updated_at`) VALUES
-(1, 'kaktus', 'kaktus adalah sebuah', '2022-04-11 13:29:44', '2022-04-11 13:29:49'),
-(3, 'sukulen', 'sukulen adalah', '2022-04-11 13:30:45', '2022-04-11 13:30:45'),
-(4, 'news', 'baru2022', '2022-04-11 16:47:02', '2022-04-11 16:47:02'),
-(5, 'aca', 'apa itudah', '2022-04-11 18:47:33', '2022-04-15 03:09:17');
+(1, 'kaktus', 'Apa yang dimaksud dengan tanaman kaktus? Kata kaktus berasal dari bahasa latin \"kaktos\" yang berarti tumbuhan berduri yang identitasnya tidak pasti. Kaktus diakui akan daya tahan dan kemampuannya untuk berkembang di lingkungan yang sulit, mereka diklasifikasikan sebagai sukulen batang karena mereka memiliki batang berdaging khusus', '2022-04-11 13:29:44', '2022-05-27 12:35:30'),
+(3, 'sukulen', 'Tumbuhan sukulen, juga dikenal sebagai sukulen, adalah tumbuhan dengan organ yang menebal, berdaging, dan membengkak, biasanya untuk menyimpan air di iklim atau kondisi tanah kering.', '2022-04-11 13:30:45', '2022-05-27 12:35:52'),
+(4, 'Aglaonema', 'Aglaonema adalah tanaman hias populer dari suku talas-talasan atau Araceae. Genus Aglaonema memiliki sekitar 30 spesies. Mereka berasal dari daerah tropis dan subtropis di Asia dan Nugini. Mereka umumnya dikenal sebagai Chinese', '2022-04-11 16:47:02', '2022-05-27 12:36:39'),
+(5, 'Keladi hias', 'Keladi merupakan sekelompok tumbuhan dari genus Caladium. Dalam bahasa sehari-hari keladi kerap juga dipakai untuk menyebut beberapa tumbuhan lain yang masih sekerabat namun tidak termasuk Caladium, seperti talas. Keladi sejati jarang membentuk umbi yang membesar.', '2022-04-11 18:47:33', '2022-05-27 12:37:17'),
+(9, 'anggrek', 'Suku anggrek-anggrekan merupakan satu suku tumbuhan berbunga dengan anggota jenis terbanyak. Jenis-jenisnya tersebar luas dari daerah tropika basah hingga wilayah sirkumpolar, meskipun sebagian besar anggotanya ditemukan di daerah tropika.', '2022-05-27 12:38:07', '2022-05-27 12:38:07'),
+(10, 'Lainnya', 'Product Test', '2022-05-27 13:46:11', '2022-05-27 13:46:11');
 
 -- --------------------------------------------------------
 
@@ -258,7 +259,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (14, '2022_04_19_061257_update_products_table', 1),
 (15, '2022_05_11_013856_create_transaksi_table', 1),
 (16, '2022_05_22_045812_create_article_comments_table', 1),
-(17, '2022_05_22_055252_update_article_comments_table', 1);
+(17, '2022_05_22_055252_update_article_comments_table', 1),
+(18, '2022_05_27_125605_create_product_images_table', 2);
 
 -- --------------------------------------------------------
 
@@ -338,12 +340,56 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `code`, `category`, `nameProduct`, `description`, `image`, `created_at`, `updated_at`, `priceRange`) VALUES
-(2, '80842', 'kaktus', 'Fancy Product', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae condimentum erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed posuere, purus at efficitur hendrerit, augue elit lacinia arcu, a eleifend sem elit et nunc. Sed rutrum vestibulum est, sit amet cursus dolor fermentum vel. Suspendisse mi nibh,', 'image.jpg', '2022-04-11 17:21:06', '2022-04-18 23:25:18', 'Rp10000 - Rp30000'),
-(5, '80841', 'sukulen', 'Special Item', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae condimentum erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed posuere, purus at efficitur hendrerit, augue elit lacinia arcu, a eleifend sem elit et nunc. Sed rutrum vestibulum est, sit amet cursus dolor fermentum vel. Suspendisse mi nibh,', 'image.jpg', '2022-04-11 18:05:01', '2022-04-18 23:25:32', 'Rp20000 - Rp40000'),
-(6, '4202', 'kaktus', 'Sale Item', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae condimentum erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed posuere, purus at efficitur hendrerit, augue elit lacinia arcu, a eleifend sem elit et nunc. Sed rutrum vestibulum est, sit amet cursus dolor fermentum vel. Suspendisse mi nibh,', 'image.jpg', '2022-04-12 19:32:25', '2022-04-18 23:26:15', 'Rp10000 - Rp30000'),
-(7, '8082', 'kaktus', 'Fancy Product', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae condimentum erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed posuere, purus at efficitur hendrerit, augue elit lacinia arcu, a eleifend sem elit et nunc. Sed rutrum vestibulum est, sit amet cursus dolor fermentum vel. Suspendisse mi nibh,', 'image.jpg', '2022-04-11 17:21:06', '2022-04-18 23:26:27', 'Rp10000 - Rp30000'),
-(8, '8041', 'sukulen', 'Special Item', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae condimentum erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed posuere, purus at efficitur hendrerit, augue elit lacinia arcu, a eleifend sem elit et nunc. Sed rutrum vestibulum est, sit amet cursus dolor fermentum vel. Suspendisse mi nibh,', 'image.jpg', '2022-04-11 18:05:01', '2022-04-18 23:26:38', 'Rp20000 - Rp40000'),
-(9, '422', 'kaktus', 'Sale Item', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae condimentum erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed posuere, purus at efficitur hendrerit, augue elit lacinia arcu, a eleifend sem elit et nunc. Sed rutrum vestibulum est, sit amet cursus dolor fermentum vel. Suspendisse mi nibh,', 'image.jpg', '2022-04-12 19:32:25', '2022-04-18 23:26:49', 'Rp10000 - Rp30000');
+(13, 'K001', 'kaktus', 'Kaktus', 'Kata jamak untuk kaktus adalah kakti. Kaktus memiliki akar yang panjang untuk mencari air dan memperlebar penyerapan air dalam tanah.', 'post-images/JYhttNHBgAQfcfPIbBU1fx2E2t5U7boV8X3dwYDK.jpg', '2022-05-27 12:42:26', '2022-05-27 12:42:26', 'Rp10000 - Rp100000'),
+(14, 'S001', 'sukulen', 'Sukulen', 'Tumbuhan sukulen, juga dikenal sebagai sukulen, adalah tumbuhan dengan organ yang menebal, berdaging, dan membengkak, biasanya untuk menyimpan air di iklim atau kondisi tanah kering.', 'post-images/fDQwTrAgd2UO4OgoGd1VSnChOqmeEbchHvaV82ib.jpg', '2022-05-27 12:43:52', '2022-05-27 12:45:20', 'Rp.20000 - Rp.30000'),
+(15, 'A001', 'Aglaonema', 'Aglaonema', 'Macam-macam tanaman hias daun yang sedang hits saat ini adalah aglaonema. Aglaonema termasuk tanaman hias jenis talas-talasan. Ada banyak jenis aglaonema dengan beragam corak, warna, dan ukuran. Harga aglaonema pun beragam sesuai dengan jenisnya, mulai dari RP 30 ribu sampai jutaan rupiah', 'post-images/SdcIssFilcyfi2IR63kjiU1I5Sff5EhtQBDmekix.jpg', '2022-05-27 12:45:05', '2022-05-27 12:45:05', 'Rp20000 - Rp40000'),
+(16, 'Ke001', 'Keladi hias', 'Keladi', 'Keladi merupakan sekelompok tumbuhan dari genus Caladium. Dalam bahasa sehari-hari keladi kerap juga dipakai untuk menyebut beberapa tumbuhan lain yang masih sekerabat namun tidak termasuk Caladium, seperti talas. Keladi sejati jarang membentuk umbi yang membesar.', 'post-images/gHu9bHwEml1Yb033PAGKXW2KLYLnYZgu3MgZuFO7.jpg', '2022-05-27 12:47:21', '2022-05-27 12:47:21', 'Rp20000 - Rp50000'),
+(17, 'AN001', 'anggrek', 'Anggrek', 'Suku anggrek-anggrekan merupakan satu suku tumbuhan berbunga dengan anggota jenis terbanyak. Jenis-jenisnya tersebar luas dari daerah tropika basah hingga wilayah sirkumpolar, meskipun sebagian besar anggotanya ditemukan di daerah', 'post-images/BYy9ifH0ipWFtW6KhrRl2ZidnJ5AlUsGbcX0I1Po.jpg', '2022-05-27 12:48:35', '2022-05-27 12:48:35', 'Rp20000 - Rp50000'),
+(18, 'L001', 'Lainnya', 'Sepatu', 'Sepatu Safety', 'post-images/X5t1P7VntR75noUD5CELUubzHucydeJs8LLbFonU.jpg', '2022-05-27 13:47:17', '2022-05-27 13:47:17', 'Rp100000-Rp1000000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_images`
+--
+
+CREATE TABLE `product_images` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `code` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `product_images`
+--
+
+INSERT INTO `product_images` (`id`, `code`, `image`, `created_at`, `updated_at`) VALUES
+(1, 'K001', 'post-images/rjAlsKvYWJ1nUBvqWeCmdoHx22Q18aEL5S9foJTs.jpg', '2022-05-27 13:30:06', '2022-05-27 13:30:06'),
+(4, 'K001', 'post-images/mqpIQabw3bzz0nISpWMwgHaCsiXGIjPMOh18ee6A.jpg', '2022-05-27 13:42:37', '2022-05-27 13:42:37'),
+(5, 'K001', 'post-images/BsVNgegc7pxO1XL1LgxAqmBIcDPwwOiYu0SBHMxo.jpg', '2022-05-27 13:44:10', '2022-05-27 13:44:10'),
+(6, 'K001', 'post-images/pDmO2rKwudpLlTZOV4hQL7pz176vnCOMiAjzcVoB.jpg', '2022-05-27 13:44:41', '2022-05-27 13:44:41'),
+(7, 'L001', 'post-images/tDjW0GQ0iKaEz86MuJeZ0wPsLT1dRpX0s6JHQ3hF.jpg', '2022-05-27 13:47:28', '2022-05-27 13:47:28'),
+(8, 'L001', 'post-images/k4eixgb1vw5sRojBiMZem44dn2FUSJi9NHQF3GYm.jpg', '2022-05-27 13:47:36', '2022-05-27 13:47:36'),
+(9, 'L001', 'post-images/sSQVCGNvodxKXah9ZerzYfoCqhlkwMLPtaLwlvNk.jpg', '2022-05-27 13:47:45', '2022-05-27 13:47:45'),
+(10, 'L001', 'post-images/ZCG9k0mpWFlH3Rej3uCDt36JgVGNfgVBNFPN24BH.jpg', '2022-05-27 13:47:58', '2022-05-27 13:47:58'),
+(11, 'S001', 'post-images/EMuP4s2IUQsIHuVAdaD8R6tBzBbXOI350HVsc95Y.jpg', '2022-05-27 13:49:51', '2022-05-27 13:49:51'),
+(12, 'S001', 'post-images/nLwIEp2rrYTMFN5UbZVPEbT1ou6kutM7ih69Tcw4.jpg', '2022-05-27 13:50:09', '2022-05-27 13:50:09'),
+(13, 'S001', 'post-images/dDmJ1BCyLH1DfRzQdtcmZ5eBIa5OaE4xrY4wImC8.jpg', '2022-05-27 13:50:16', '2022-05-27 13:50:16'),
+(14, 'S001', 'post-images/3uC8hiotThlsBmZqVYloi1nAY9wj11nMjGS679Ut.jpg', '2022-05-27 13:50:21', '2022-05-27 13:50:21'),
+(15, 'A001', 'post-images/5nKSRAnLkhbN2MjhAWjCJAFbX8bKuFUC6VsfUl83.png', '2022-05-27 13:52:40', '2022-05-27 13:52:40'),
+(16, 'A001', 'post-images/fJDTS0Jj5gkE8gmldHmE5wNqLHCCxYmSq7BQuMT1.webp', '2022-05-27 13:52:48', '2022-05-27 13:52:48'),
+(17, 'A001', 'post-images/9DDivP9wtI5eniR2b5ULsKiO5R9InBJT06xyllLp.jpg', '2022-05-27 13:53:10', '2022-05-27 13:53:10'),
+(18, 'A001', 'post-images/1I7w2tXpDfyxYE2C5yHsHBkELjxNATdB0QwhddJ3.jpg', '2022-05-27 13:53:22', '2022-05-27 13:53:22'),
+(19, 'Ke001', 'post-images/AOnEA87mSFVUBNWoHjT4dPbMymWWmlWMdGluaD76.jpg', '2022-05-27 13:55:21', '2022-05-27 13:55:21'),
+(20, 'Ke001', 'post-images/LvNEU14pOKKE8h0l8qaE7nAkCBEvEQlMWML9RJrB.png', '2022-05-27 13:55:28', '2022-05-27 13:55:28'),
+(21, 'Ke001', 'post-images/jlnxFyqJrj7HSW22L2dP0JXB52zfaOAe7qSYu3o7.jpg', '2022-05-27 13:55:39', '2022-05-27 13:55:39'),
+(22, 'Ke001', 'post-images/eOWXP27eGfZbVGG9a0FN1AxLrEigLKrVgUtbEyrl.jpg', '2022-05-27 13:55:50', '2022-05-27 13:55:50'),
+(23, 'AN001', 'post-images/IWpP2sTWiJbIDwBOqAAjPnihvuFEEuecb0f0INOf.jpg', '2022-05-27 13:57:10', '2022-05-27 13:57:10'),
+(24, 'AN001', 'post-images/xWR0JZdqm0i1x8CJgvulFBUBMrOrr23UfUkXyAAa.jpg', '2022-05-27 13:57:18', '2022-05-27 13:57:18'),
+(25, 'AN001', 'post-images/wg42DWHRLfw3YWxYzTGbiJQMajyDP6PaQmuqtwB7.jpg', '2022-05-27 13:57:29', '2022-05-27 13:57:29'),
+(26, 'AN001', 'post-images/9XR8uFZuFpP3ZKsAhNkrwAjn61Sk73UOfEXiJxbs.jpg', '2022-05-27 13:57:39', '2022-05-27 13:57:39');
 
 -- --------------------------------------------------------
 
@@ -366,24 +412,24 @@ CREATE TABLE `stocks` (
 --
 
 INSERT INTO `stocks` (`id`, `code`, `size`, `stock`, `price`, `created_at`, `updated_at`) VALUES
-(7, '80842', 'small', 10, 100000.00, '2022-04-18 07:26:27', '2022-04-18 07:26:27'),
-(8, '80842', 'standart', 10, 200000.00, '2022-04-18 07:26:38', '2022-04-18 07:26:38'),
-(9, '80842', 'big', 10, 300000.00, '2022-04-18 07:26:52', '2022-04-18 07:26:52'),
-(10, '80841', 'small', 10, 200000.00, '2022-04-18 07:27:04', '2022-04-18 07:27:04'),
-(11, '80841', 'standart', 10, 300000.00, '2022-04-18 07:27:21', '2022-04-18 07:27:21'),
-(12, '80841', 'big', 10, 400000.00, '2022-04-18 07:27:40', '2022-04-18 07:27:40'),
-(13, '4202', 'small', 10, 100000.00, '2022-04-18 07:26:27', '2022-04-18 07:28:54'),
-(14, '4202', 'standart', 10, 200000.00, '2022-04-18 07:26:38', '2022-04-18 07:29:03'),
-(15, '4202', 'big', 10, 300000.00, '2022-04-18 07:26:52', '2022-04-18 07:29:09'),
-(16, '8041', 'small', 10, 200000.00, '2022-04-18 07:27:04', '2022-04-18 07:29:41'),
-(17, '8041', 'standart', 10, 300000.00, '2022-04-18 07:27:21', '2022-04-18 07:29:50'),
-(18, '8041', 'big', 10, 400000.00, '2022-04-18 07:27:40', '2022-04-18 07:29:57'),
-(19, '8082', 'small', 10, 100000.00, '2022-04-18 07:26:27', '2022-04-18 07:30:33'),
-(20, '8082', 'standart', 10, 200000.00, '2022-04-18 07:26:38', '2022-04-18 07:30:38'),
-(21, '8082', 'big', 10, 300000.00, '2022-04-18 07:26:52', '2022-04-18 07:30:42'),
-(22, '422', 'small', 10, 100000.00, '2022-04-18 07:26:27', '2022-04-18 07:30:33'),
-(23, '422', 'standart', 10, 200000.00, '2022-04-18 07:26:38', '2022-04-18 07:30:38'),
-(24, '422', 'big', 10, 300000.00, '2022-04-18 07:26:52', '2022-04-18 07:30:42');
+(7, 'K001', 'small', 10, 100000.00, '2022-04-18 07:26:27', '2022-04-18 07:26:27'),
+(8, 'K001', 'standart', 10, 200000.00, '2022-04-18 07:26:38', '2022-04-18 07:26:38'),
+(9, 'K001', 'big', 10, 300000.00, '2022-04-18 07:26:52', '2022-04-18 07:26:52'),
+(10, 'S001', 'small', 10, 200000.00, '2022-04-18 07:27:04', '2022-04-18 07:27:04'),
+(11, 'S001', 'standart', 10, 300000.00, '2022-04-18 07:27:21', '2022-04-18 07:27:21'),
+(12, 'S001', 'big', 10, 400000.00, '2022-04-18 07:27:40', '2022-04-18 07:27:40'),
+(13, 'A001', 'small', 10, 100000.00, '2022-04-18 07:26:27', '2022-04-18 07:28:54'),
+(14, 'A001', 'standart', 10, 200000.00, '2022-04-18 07:26:38', '2022-04-18 07:29:03'),
+(15, 'A001', 'big', 10, 300000.00, '2022-04-18 07:26:52', '2022-04-18 07:29:09'),
+(16, 'Ke001', 'small', 10, 200000.00, '2022-04-18 07:27:04', '2022-04-18 07:29:41'),
+(17, 'Ke001', 'standart', 10, 300000.00, '2022-04-18 07:27:21', '2022-04-18 07:29:50'),
+(18, 'Ke001', 'big', 10, 400000.00, '2022-04-18 07:27:40', '2022-04-18 07:29:57'),
+(19, 'AN001', 'small', 10, 100000.00, '2022-04-18 07:26:27', '2022-04-18 07:30:33'),
+(20, 'AN001', 'standart', 10, 200000.00, '2022-04-18 07:26:38', '2022-04-18 07:30:38'),
+(21, 'AN001', 'big', 10, 300000.00, '2022-04-18 07:26:52', '2022-04-18 07:30:42'),
+(22, 'L001', 'small', 10, 100000.00, '2022-04-18 07:26:27', '2022-04-18 07:30:33'),
+(23, 'L001', 'standart', 10, 200000.00, '2022-04-18 07:26:38', '2022-04-18 07:30:38'),
+(24, 'L001', 'big', 10, 300000.00, '2022-04-18 07:26:52', '2022-04-18 07:30:42');
 
 -- --------------------------------------------------------
 
@@ -519,6 +565,12 @@ ALTER TABLE `products`
   ADD UNIQUE KEY `products_code_unique` (`code`);
 
 --
+-- Indexes for table `product_images`
+--
+ALTER TABLE `product_images`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `stocks`
 --
 ALTER TABLE `stocks`
@@ -558,13 +610,13 @@ ALTER TABLE `articles`
 -- AUTO_INCREMENT for table `carts`
 --
 ALTER TABLE `carts`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -588,7 +640,7 @@ ALTER TABLE `members`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -606,7 +658,13 @@ ALTER TABLE `posters`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `product_images`
+--
+ALTER TABLE `product_images`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `stocks`
